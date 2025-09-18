174b8765f81f086a680e5bb712f3e8d68f4be6e4
morarasospeter <morarasospeter01@gmail.com>
2025-09-18 14:04:08 +0300

    Update urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
+    path('', lambda request: redirect('medicine_list')),  # redirect homepage → /medicines/
     path('medicines/', views.medicine_list, name='medicine_list'),
     path('add/', views.medicine_add, name='medicine_add'),
     path('edit/<int:id>/', views.medicine_edit, name='medicine_edit'),
